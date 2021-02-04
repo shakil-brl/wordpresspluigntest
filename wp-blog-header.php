7974cfcfc4d0559c288ac56fa68df6afe569a645
wizdore <shada.kaalo@gmail.com>
2021-02-04 15:59:59 +0600

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -12,7 +12,7 @@ if ( ! isset( $wp_did_header ) ) {
 	// Load the WordPress library.
 	require_once __DIR__ . '/wp-load.php';
 
-	// Set up the WordPress query.
+	// Set up the WordPress query.hh
 	wp();
 
 	// Load the theme template.
